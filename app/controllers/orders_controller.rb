c26fc3e05d063ad6e15295fc636d52205049774e
Chris <tw.chrislyu@gmail.com>
2017-04-06 15:03:23 +0800

    Generate orders

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,39 @@
+class OrdersController < ApplicationController
+	before_action :authenticate_user!
+
+  def create
+    @order = current_user.orders.build(order_params)
+
+    if @order.save
+      @order.build_item_cache_from_cart(current_cart)
+      @order.calculate_total!(current_cart)
+      redirect_to order_path(@order)
+    else
+      render "carts/checkout"
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(info_attributes: [:billing_name,
+                                                    :billing_address,
+                                                    :shipping_name,
+                                                    :shipping_address] )
+  end
+
+  def build_item_cache_from_cart(cart)
+    cart.items.each do |cart_item|
+      item = items.build
+      item.product_name = cart_item.title
+      item.quantity = 1
+      item.price = cart_item.price
+      item.save
+    end
+  end
+
+  def calculate_total!(cart)
+    self.total = cart.total_price
+    self.save
+  end
+end
--- app/models/order_info.rb
+++ app/models/order_info.rb
@@ -1,3 +1,8 @@
 class OrderInfo < ApplicationRecord
 	belongs_to :order
+
+	validates :billing_name,     presence: true
+  validates :billing_address,  presence: true
+  validates :shipping_name,    presence: true
+  validates :shipping_address, presence: true
 end
