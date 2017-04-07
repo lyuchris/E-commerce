14c5a1eceba7ff99a200f759ac20cd7d11b5562c
Chris <tw.chrislyu@gmail.com>
2017-04-07 22:50:41 +0800

    add i18n

--- app/models/order_info.rb
+++ app/models/order_info.rb
@@ -1,7 +1,7 @@
 class OrderInfo < ApplicationRecord
-	belongs_to :order
+  belongs_to :order
 
-	validates :billing_name,     presence: true
+  validates :billing_name,     presence: true
   validates :billing_address,  presence: true
   validates :shipping_name,    presence: true
   validates :shipping_address, presence: true
