66e68e996f8f1b8a1de68b47161e3e65accf9dc8
Chris <tw.chrislyu@gmail.com>
2017-04-07 11:05:51 +0800

    admin_all orders in the system

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   namespace :admin do
     resources :products
+    resources :orders
     resources :users do
       member do
         post :to_admin
